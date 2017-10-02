b9dd3099c0362e3912cc7eff7adeb12aad9e7a79
DUC Tran Huynh <DUC.TH2398@SINHVIEN.HOASEN.EDU.VN>
2017-10-02 14:13:35 +0700

    loi hoten

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,28 @@
 #include <conio.h>
 #include <stdio.h>
 
+
+struct SV
+{
+	char hoten[];
+	float toan, ly, hoa;
+	char mssv;
+};
+void nhapmang()
+{
+	int n;
+	printf("nhap n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("nhap ho ten");
+		scanf("%")
+	}
+
+}
+
+
+
+
 void main()
 {
 	printf("hi");
